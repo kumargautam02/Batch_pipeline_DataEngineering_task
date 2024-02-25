14ae327350f51870ad05962de873b77992b70f68
Gautam <gautam.kumar410305@gmail.com>
2024-02-26 05:21:11 +0530

    updating scripts

--- database_script.py
+++ database_script.py
@@ -2,16 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy import text
 from sqlalchemy.exc import SQLAlchemyError
 
-path = ("sqlite:///D:/DataSlush_project/database/cricket.db")
+path = ("sqlite:///C:/Users/Admin/Downloads/procesing/Batch_pipeline_DataEngineering_task/database/cricket.db")
 
 my_conn = create_engine(path)
 my_conn = my_conn.connect()
-
-
-
 try:
     
-
     result = my_conn.execute(text("select name from sqlite_master"))
     print(result.all())
 except SQLAlchemyError as e:
