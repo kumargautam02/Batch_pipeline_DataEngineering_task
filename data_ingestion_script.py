6bb4c27919c52b7c6b3c86f0f57fd8364dafddd5
Gautam <gautam.kumar410305@gmail.com>
2024-02-26 17:27:07 +0530

    added ingestion_script

--- data_ingestion_script.py
+++ data_ingestion_script.py
@@ -1,28 +1,26 @@
-""""This is to ingest data from the cricsheet"""
+# """"This is to ingest data from the cricsheet"""
 from selenium import webdriver
 from selenium.webdriver import chrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-
 from selenium.webdriver.common.keys import Keys
- 
-# def data_ingestion(type_of_match):
-options= webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
-# create webdriver object
-driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
-
-# get google.co.in
-driver.get("https://cricsheet.org/downloads/")
+from selenium.webdriver.chrome.options import Options
 
 
-# search_bar = driver.find_element_by_name("q")
-
-# element = driver.find_element(By.XPATH, '//*[@id="main"]/div/table[2]/tbody/tr[5]/td[1]')
-# name
-login_form = driver.find_element(By.ID ,"name")
-print(login_form)
-
-
-# data_ingestion("")
+def download_required_files(logger):
+    chromeOptions= webdriver.ChromeOptions()
+    chromeOptions.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(options = chromeOptions,service=Service(ChromeDriverManager().install()))
+    driver.get("https://cricsheet.org/downloads/")
+    x_path_for_male_dataset = '//*[@id="main"]/div/table[2]/tbody/tr[5]/td[4]/ul/li[1]/a'
+    download_mail_dataset = driver.find_element(By.XPATH, x_path_for_male_dataset)
+    download_mail_dataset.click()
+    driver.implicitly_wait(60)
+    logger.info("Download of Male_Dataset Started")
+    x_path_for_female_dataset = '//*[@id="main"]/div/table[2]/tbody/tr[5]/td[3]/ul/li[1]/a'
+    download_female_dataset = driver.find_element(By.XPATH, x_path_for_female_dataset)
+    download_female_dataset.click()
+    driver.implicitly_wait(60)
+    logger.info("Download of Female_Dataset Started")
+    return 
